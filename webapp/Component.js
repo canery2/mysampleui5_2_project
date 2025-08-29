3165c5c460b0fb21810312071348dd9be34627c8
Caner Yuksel <caner_yukselbm@hotmail.com>
2025-08-29 16:12:43 +0300

    first commit

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,9 +1,25 @@
-sap.ui.define(["sap/ui/core/UIComponent", "sap/ui/core/ComponentSupport"], (UIComponent) => {
+sap.ui.define(["sap/ui/core/UIComponent",
+	"sap/ui/model/json/JSONModel"
+], (UIComponent,JSONModel) => {
 	"use strict";
-	return UIComponent.extend("sap.ui.demo.todo.Component", {
+	return UIComponent.extend("sample-app.Component", {
 		metadata: {
-			manifest: "json",
-			interfaces: ["sap.ui.core.IAsyncContentCreation"],
-		}
+			
+			manifest: "json"
+			//interfaces: ["sap.ui.core.IAsyncContentCreation"],
+		},
+		init : function () {
+			// call the init function of the parent
+			UIComponent.prototype.init.apply(this, arguments);
+
+			const oData = {
+				recipient : {
+				   name : "World"
+				}
+			 };
+			 const oModel = new JSONModel(oData);
+			 this.setModel(oModel); 
+	   }
+		
 	});
 });
---
+++ webapp/controller/HelloPanel.controller.js
@@ -0,0 +1,55 @@
+/* sap.ui.define(["sap/ui/core/mvc/Controller",
+               "sap/m/MessageToast"
+], function (Controller,MessageToast){
+    "use strict";
+    return Controller.extend("sample-app.controller.HelloPanel",{
+        onShowHello: function() {
+            const oBundle = this.getView().getModel("i18n").getResourceBundle();
+            const sRecipint = this.getView().getModel().getProperty("/recipint/name");
+            const sMsg = oBundle.getText("helloMsg",[sRecipint]);
+
+            MessageToast.show(sMsg);
+        }
+    })
+}) */
+
+    sap.ui.define([
+        "sap/ui/core/mvc/Controller",
+        "sap/m/MessageToast"
+     ], function (Controller, MessageToast) {
+        "use strict";
+        return Controller.extend("sample-app.controller.HelloPanel", {
+           /* onShowHello: function () {
+              // read msg from i18n model
+              const oBundle = this.getView().getModel("i18n").getResourceBundle();
+              const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+              const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+              // show message
+              MessageToast.show(sMsg);
+           } */
+              onShowHello: function () {
+                const oBundlePromise = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+                oBundlePromise.then((oBundle) => {
+                    const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+                    const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+                    MessageToast.show(sMsg);
+                });
+                
+                console.log(oBundlePromise); 
+
+            },
+            _getDialog : function () {
+                if (!this._oDialog) {
+                   this._oDialog = sap.ui.xmlfragment("sample-app.view.HelloDialog",this);
+                   this.getView().addDependent(this._oDialog);
+                }
+                return this._oDialog;
+             },
+             onOpenDialog : function () {
+                this._getDialog().open();
+             },
+             onCloseDialog : function () {
+                this._getDialog().close();
+             }
+        });
+     });
