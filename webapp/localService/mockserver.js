f61a035c7f0952dc62cace3bf6735010d5c5e86d
Caner Yuksel <caner_yukselbm@hotmail.com>
2025-09-03 13:39:39 +0300

    Integration testleri yapıldı

--- webapp/localService/mockserver.js
+++ webapp/localService/mockserver.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 sap.ui.define([
 	"sap/ui/core/util/MockServer"
 ], function (MockServer) {
@@ -8,7 +9,6 @@ sap.ui.define([
 			const oMockServer = new MockServer({
 				rootUri: "/destinations/northwind/V2/Northwind/Northwind.svc/"
 			}); 
-			// eslint-disable-next-line no-undef
 			const oUriParameters = jQuery.sap.getUriParameters();
 			// configure
 			MockServer.config({
@@ -16,7 +16,6 @@ sap.ui.define([
 				autoRespondAfter: oUriParameters.get("serverDelay") || 1000
 			});
 			// simulate
-			// eslint-disable-next-line no-undef
 			const sPath = jQuery.sap.getModulePath("sample-app.localService");
 			oMockServer.simulate(sPath + "/metadata.xml", sPath + "/mockdata");
 			// start
---
+++ webapp/test/integration/navigationJourney.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+sap.ui.require([
+	"sap/ui/test/opaQunit"
+], function () {
+	"use strict";
+	QUnit.module("Navigation");
+	opaTest("Should open the hello dialog", function (Given, When, Then) {
+		// Arrangements
+		Given.iStartMyAppInAFrame(jQuery.sap.getResourcePath("sample-app/test", ".html"));
+		//Actions
+		When.onTheAppPage.iPressTheSayHelloWithDialogButton();
+		// Assertions
+		Then.onTheAppPage.iShouldSeeTheHelloDialog().
+			and.iTeardownMyAppFrame();
+	});
+});
