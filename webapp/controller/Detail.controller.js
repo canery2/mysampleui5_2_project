2e7f057c78da54b63a602acb8ca10dfccad46689
Caner Yuksel <caner_yukselbm@hotmail.com>
2025-09-04 16:17:02 +0300

    Detail sayfası detayı eklendi

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,6 +1,7 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/routing/History"
+], function (Controller,History) {
 	"use strict";
 	return Controller.extend("sample-app.controller.Detail", {
 		onInit: function () {
@@ -12,6 +13,19 @@ sap.ui.define([
 				path: "/" + oEvent.getParameter("arguments").invoicePath,
 				model: "invoice"
 			});
-		}
+		},
+		
+        onNavBack: function ()  {
+            const oHistory = History.getInstance();
+            const sPreviousHash = oHistory.getPreviousHash();
+
+            if (sPreviousHash !== undefined) {
+                window.history.go(-1);
+            } else {
+                const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                oRouter.navTo("overview",true);
+            }
+
+        }
 	});
 });
