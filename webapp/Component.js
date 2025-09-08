45a8eea23748836e7a02bcecd4d04b0b1f466fb9
Caner Yuksel <Cyuksel@gemak.local>
2025-09-08 13:56:30 +0300

    first commit

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,20 +1,12 @@
-/* eslint-disable no-unused-vars */
-sap.ui.define(
-	[
-		"sap/ui/core/UIComponent",
-		"sap/ui/model/json/JSONModel",
-		"sample-app/controller/HelloDialog",
-		"sap/ui/Device",
-	],
-	function (UIComponent, JSONModel, HelloDialog, Device) {
-		"use strict";
-		return UIComponent.extend("sample-app.Component", {
-			metadata: {
-				manifest: "json",
-			},
-			init: function () {
-				// call the init function of the parent
-			}
-		});
-	}
-);
+sap.ui.define([
+	"sap/ui/core/UIComponent"
+], (UIComponent) => {
+	"use strict";
+
+	return UIComponent.extend("sample-app-copy.Component", {
+		metadata: {
+			interfaces: ["sap.ui.core.IAsyncContentCreation"],
+			manifest: "json"
+		}
+	});
+});
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,15 +1,17 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
-	"use strict";
+    "sap/ui/core/mvc/Controller",
+    "sap/m/URLHelper"
+], function (Controller, URLHelper) {
+    "use strict";
 
-	return Controller.extend("sample-app.controller.App", {
-
-		onInit: function () {
-			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
-		},
-		onOpenDialog: function () {
-			this.getOwnerComponent().helloDialog.open(this.getView());
-		}
-	});
+    return Controller.extend("sample-app-copy.controller.App", {
+        formatMail: function (sFirstName, sLastName) {
+            const oBundle = this.getView().getModel("i18n").getResourceBundle();
+            return URLHelper.normalizeEmail(
+                sFirstName + "." + sLastName + "@example.com",
+                oBundle.getText("mailSubject", [sFirstName]),
+                oBundle.getText("mailBody")
+            );
+        }
+    });
 });
