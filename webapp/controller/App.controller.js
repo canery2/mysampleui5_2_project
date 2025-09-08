8cc7a4579378a508f5d37e7889d01669e23f1632
Caner Yuksel <Cyuksel@gemak.local>
2025-09-08 14:09:38 +0300

    Data binding1

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,17 +1,17 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/URLHelper"
-], function (Controller, URLHelper) {
-    "use strict";
+	"sap/m/library",
+	"sap/ui/core/mvc/Controller"
+], (mobileLibrary, Controller) => {
+	"use strict";
 
-    return Controller.extend("sample-app-copy.controller.App", {
-        formatMail: function (sFirstName, sLastName) {
-            const oBundle = this.getView().getModel("i18n").getResourceBundle();
-            return URLHelper.normalizeEmail(
-                sFirstName + "." + sLastName + "@example.com",
-                oBundle.getText("mailSubject", [sFirstName]),
-                oBundle.getText("mailBody")
-            );
-        }
-    });
+	return Controller.extend("sample-app-copy.controller.App", {
+		formatMail(sFirstName, sLastName) {
+			const oBundle = this.getView().getModel("i18n").getResourceBundle();
+
+			return mobileLibrary.URLHelper.normalizeEmail(
+				`${sFirstName}.${sLastName}@example.com`,
+				oBundle.getText("mailSubject", [sFirstName]),
+				oBundle.getText("mailBody"));
+		}
+	});
 });
